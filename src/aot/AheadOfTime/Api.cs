040862d01124eb4ebc9fe671c16e0cfc09713856
maximiliysiss <zim.ma2010@yandex.ru>
2024-11-14 00:14:51 +0100

    add aot sample

---
+++ src/aot/AheadOfTime/Api.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AheadOfTime;
+
+public static class Api
+{
+    [UnmanagedCallersOnly(EntryPoint = "Calculate")]
+    public static int Calculate(int a, int b) => a + b;
+}
